f421fdbf09aeb6718c0a1d666471e6f4b6101798
popipopka <eg.belykh@yandex.ru>
2024-01-06 20:51:20 +0300

    Create a base

--- src/main/java/com/meow/bebrablender/BebraApplication.java
+++ src/main/java/com/meow/bebrablender/BebraApplication.java
@@ -3,16 +3,24 @@ package com.meow.bebrablender;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class BebraApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BebraApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        AnchorPane viewport = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("gui.fxml")));
+
+        Scene scene = new Scene(viewport);
+        stage.setMinWidth(1600);
+        stage.setMinHeight(900);
+        viewport.prefWidthProperty().bind(scene.widthProperty());
+        viewport.prefHeightProperty().bind(scene.heightProperty());
+
+        stage.setTitle("Simple3DViewer");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/meow/bebrablender/model/Model.java
@@ -0,0 +1,14 @@
+package com.meow.bebrablender.model;
+
+import com.meow.bebrablender.math.Vector2f;
+import com.meow.bebrablender.math.Vector3f;
+
+import java.util.ArrayList;
+
+public class Model {
+
+    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
+    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
+    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
+    public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+}
