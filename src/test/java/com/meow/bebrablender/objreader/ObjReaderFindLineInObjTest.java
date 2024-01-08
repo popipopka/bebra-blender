2026eb2a89857f54bc145dc6e8c8725259bda409
popipopka <eg.belykh@yandex.ru>
2024-01-08 18:10:58 +0300

    Work with ObjReader: tests and class

--- src/test/java/com/meow/bebrablender/objreader/ObjReaderFindLineInObjTest.java
+++ src/test/java/com/meow/bebrablender/objreader/ObjReaderFindLineInObjTest.java
@@ -9,19 +9,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ObjReaderFindLineInObjTest  {
-    private final ObjReader reader = new ObjReader(Path.of("src/main/ObjFiles/TestFindLineInObj.obj"));
+    private final ObjReader reader = new ObjReader(Path.of("src/main/resources/com/meow/bebrablender/ObjFiles/Null.obj"));
 
     ObjReaderFindLineInObjTest() throws IOException {
     }
 
     @Test
-    void testFindLine01() throws IOException{
+    void testFindLine01() {
         int expected = reader.findLineInObj("v", 1);
         Assertions.assertEquals(expected, 1);
     }
 
     @Test
-    void testFindLine02() throws IOException{
+    void testFindLine02() {
         int expected = reader.findLineInObj("v", 0);
         Assertions.assertEquals(expected, -1);
     }
@@ -33,54 +33,54 @@ class ObjReaderFindLineInObjTest  {
     }
 
     @Test
-    void testFindLine04() throws IOException{
+    void testFindLine04() {
         int expected = reader.findLineInObj("v", 6);
         Assertions.assertEquals(expected, -1);
     }
 
     @Test
-    void testFindLine05() throws IOException{
+    void testFindLine05() {
         int expected = reader.findLineInObj("vt", 0);
         Assertions.assertEquals(expected, -1);
     }
 
     @Test
-    void testFindLine06() throws IOException{
+    void testFindLine06() {
         int expected = reader.findLineInObj("vt", 2);
         Assertions.assertEquals(expected, 8);
     }
     @Test
-    void testFindLine07() throws IOException{
+    void testFindLine07() {
         int expected = reader.findLineInObj("vt", 1);
         Assertions.assertEquals(expected, 7);
     }
 
     @Test
-    void testFindLine08() throws IOException{
+    void testFindLine08() {
         int expected = reader.findLineInObj("v", 123);
         Assertions.assertEquals(expected, -1);
     }
 
     @Test
-    void testFindLine09() throws IOException{
+    void testFindLine09() {
         int expected = reader.findLineInObj("vn", 0);
         Assertions.assertEquals(expected, -1);
     }
 
     @Test
-    void testFindLine10() throws IOException{
+    void testFindLine10() {
         int expected = reader.findLineInObj("vn", 1);
         Assertions.assertEquals(expected, 13);
     }
 
     @Test
-    void testFindLine11() throws IOException{
+    void testFindLine11() {
         int expected = reader.findLineInObj("vn", -123);
         Assertions.assertEquals(expected, -1);
     }
 
     @Test
-    void testFindLine12() throws IOException{
+    void testFindLine12() {
         int expected = reader.findLineInObj("vn", 123);
         Assertions.assertEquals(expected, -1);
     }
