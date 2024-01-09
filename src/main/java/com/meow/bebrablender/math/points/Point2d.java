4f30fdb0016408c52e08fc25fbac2a1734e08e66
popipopka <eg.belykh@yandex.ru>
2024-01-09 09:27:43 +0300

    Add Point2d

--- src/main/java/com/meow/bebrablender/math/points/Point2d.java
+++ src/main/java/com/meow/bebrablender/math/points/Point2d.java
@@ -3,7 +3,7 @@ package com.meow.bebrablender.math.points;
 public class Point2d {
     private double x;
     private double y;
-//    private double depth;
+    private double depth;
 
     public Point2d(double x, double y) {
         this.x = x;
@@ -16,5 +16,9 @@ public class Point2d {
 
     public double getY() {
         return y;
+    }
+
+    public double getDepth() {
+        return depth;
     }
 }
